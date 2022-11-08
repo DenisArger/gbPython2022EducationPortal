87e50d8bdcc634b42998b1770a4e3997f9d6a894
DenisArger <den.arger@gmail.com>
2022-11-08 12:47:39 +0300

    add module reguster and interfaces

---
+++ modules/interface/student_interface.py
@@ -0,0 +1,20 @@
+from modules.read_data import read_data
+
+
+def student_interface():
+    while True:
+        print("")
+        print("Меню студента")
+        print("")
+        print("1 - Прсмотреть домашние задания")
+        print("9 - Выход")
+        choise = input("Введите команду: ")
+        if choise == "1":
+            print(
+                read_data(
+                    "hometasks",
+                    "all",
+                )
+            )
+        if choise == "9":
+            break
