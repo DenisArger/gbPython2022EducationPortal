6079e08cb80ac1b9546c73b51697dd9fc9689e95
DenisArger <den.arger@gmail.com>
2022-11-08 12:58:21 +0300

    edit message

--- start.py
+++ start.py
@@ -2,4 +2,4 @@ from modules.interface.login_interface import login_interface
 
 print("Добро пожаловать на наш образовательный портал")
 login_interface()
-print("До новых встреч!")
+print("Всего доброго!")
